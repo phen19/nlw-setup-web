311234177cb98fe10a19a700181410243e8286fc
Phen19 <phenriquelgsilva@gmail.com>
2023-01-26 09:44:49 -0300

    dia 1: setup inicial

---
+++ src/components/Habit.tsx
@@ -0,0 +1,9 @@
+interface HabitProps{
+    completed: number
+}
+
+export function Habit(props: HabitProps){
+    return(
+        <div className='bg-zinc-900 w-10 h-10'>{props.completed}</div>
+    )
+}
